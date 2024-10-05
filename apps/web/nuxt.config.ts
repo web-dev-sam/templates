3eee66e34e0e167516bf72637ff70e5b8c2b380a
Samuel Braun <office.samigo.a@gmail.com>
2024-10-05 17:33:32 +0200

    remove uneeded code

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -4,14 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-10-01",
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
   shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
     prefix: "",
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
     componentDir: "./components/ui",
   },
   app: {
