8e4e8a62515d44f415a4baa8488f38711cfcf4f7
Samuel Braun <sam@webry.com>
2024-10-09 17:06:08 +0200

    fix: :boom: Eliminate JavaScript at once!!!

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -1,9 +1,6 @@
-// @ts-check
-import { defineConfig } from "astro/config"
-
-import tailwind from "@astrojs/tailwind"
-
 import mdx from "@astrojs/mdx"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
